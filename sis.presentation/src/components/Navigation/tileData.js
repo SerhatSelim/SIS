68ff4f8a82471c5522b59adc20f5085ac96b9b63
Serhat Selim <serhatselim@hotmail.com.tr>
2018-08-04 15:42:21 +0300

    side bar
    add side bar

---
+++ sis.presentation/src/components/Navigation/tileData.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import LibraryBooks from '@material-ui/icons/LibraryBooks';
+import FormatListNumbered from '@material-ui/icons/FormatListNumbered';
+import LibraryAdd from '@material-ui/icons/LibraryAdd';
+import CalendarToday from '@material-ui/icons/CalendarToday';
+import Person from '@material-ui/icons/Person';
+import ReportIcon from '@material-ui/icons/Report';
+
+export const mailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <LibraryBooks />
+      </ListItemIcon>
+      <ListItemText primary="Courses" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LibraryAdd />
+      </ListItemIcon>
+      <ListItemText primary="Add Courses" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <LibraryAdd />
+      </ListItemIcon>
+      <ListItemText primary="Add Courses by Staff" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <CalendarToday />
+      </ListItemIcon>
+      <ListItemText primary="Exams" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <FormatListNumbered />
+      </ListItemIcon>
+      <ListItemText primary="Grades" />
+    </ListItem>
+  </div>
+);
+
+export const otherMailFolderListItems = (
+  <div>
+    <ListItem button>
+      <ListItemIcon>
+        <Person />
+      </ListItemIcon>
+      <ListItemText primary="My Profile" />
+    </ListItem>
+    <ListItem button>
+      <ListItemIcon>
+        <ReportIcon />
+      </ListItemIcon>
+      <ListItemText primary="Info" />
+    </ListItem>
+  </div>
+);
--- sis.presentation/src/index.js
+++ sis.presentation/src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import Button from "@material-ui/core/Button";
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-function App() {
-    return (
-      <Button variant="raised" color="primary">
-        Hello World
-      </Button>
-    );
-  }
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
